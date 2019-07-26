ae143b76e0026af93dbfe155b97683b1803296d9
Frank van Wijk <Frank.van.Wijk@vanderlande.com>
2019-07-26 18:38:11 +0200

    Add login form that does nothing yet

--- src/App.vue
+++ src/App.vue
@@ -1,197 +1,26 @@
 <template>
   <div>
-    <sui-segment emphasis="secondary" vertical>
-      <sui-container>
-        <h1 is="sui-header">
-          FarmBia! <sui-image size="small" :src="logo" />
-          <button
-            @click="post('/unlock')"
-            class="ui right floated large yellow button"
-          >
-            Unlock
-          </button>
-          <button
-            @click="post('/stop')"
-            class="ui right floated large red button"
-          >
-            <i class="bullhorn icon"></i> STOP!
-          </button>
-        </h1>
-      </sui-container>
-    </sui-segment>
-
-    <sui-segment vertical padded="very">
-      <sui-container>
-        <sui-grid columns="two">
-          <sui-grid-column>
-            <sui-form @submit.prevent="move(x, y, z, 'absolute')">
-              <h2 is="sui-header">Manual movement</h2>
-
-              <sui-grid columns="three">
-                <sui-grid-column>
-                  <sui-form-field>
-                    <label>X</label>
-                    <input type="number" v-model="x" placeholder="X" />
-                  </sui-form-field>
-                </sui-grid-column>
-                <sui-grid-column>
-                  <sui-form-field>
-                    <label>Y</label>
-                    <input type="number" v-model="y" placeholder="Y" />
-                  </sui-form-field>
-                </sui-grid-column>
-                <sui-grid-column>
-                  <sui-form-field>
-                    <label>Z</label>
-                    <input type="number" v-model="z" placeholder="Z" />
-                  </sui-form-field>
-                </sui-grid-column>
-              </sui-grid>
-
-              <p>
-                <sui-button color="green">Absolute</sui-button>
-                <sui-button @click.prevent="move(x, y, z, 'relative')"
-                  >Relative</sui-button
-                >
-              </p>
-            </sui-form>
-          </sui-grid-column>
-
-          <sui-grid-column>
-            <h2 is="sui-header">Execute sequences</h2>
-            <form class="ui form" @submit.prevent="executeSequence">
-              <div class="ui action input">
-                <select
-                  name="executeSequence"
-                  id="executeSequence"
-                  v-model="seqId"
-                  class="ui dropdown"
-                >
-                  <option :value="s.id" v-for="s in sequences" :key="s.id">{{
-                    s.name
-                  }}</option>
-                </select>
-                <sui-button color="green">Execute</sui-button>
-              </div>
-            </form>
-
-            <h2 is="sui-header">Tools</h2>
-            <form class="ui form" @submit.prevent="goToTool">
-              <div class="ui action input">
-                <select
-                  name="executeSequence"
-                  id="goToTool"
-                  v-model="toolId"
-                  class="ui dropdown"
-                >
-                  <option
-                    v-for="tool in tools"
-                    :key="tool.id"
-                    :value="tool.id"
-                    >{{ tool.name }}</option
-                  >
-                </select>
-                <sui-button color="green">Pick up tool</sui-button>
-              </div>
-            </form>
-
-            <h2 is="sui-header">Peripherals</h2>
-            <form class="ui form">
-              <sui-grid columns="two" v-for="p in peripherals" :key="p.id">
-                <sui-grid-column>
-                  <sui-form-field>
-                    <sui-checkbox
-                      :label="p.label"
-                      toggle
-                      v-model="p.value"
-                      @change="writePin(p.pin, p.value)"
-                    />
-                  </sui-form-field>
-                </sui-grid-column>
-                <sui-grid-column>
-                  <sui-label>pin {{ p.pin }}</sui-label>
-                </sui-grid-column>
-              </sui-grid>
-            </form>
-          </sui-grid-column>
-        </sui-grid>
-      </sui-container>
-    </sui-segment>
+    <control-room v-if="loggedIn"></control-room>
+    <login-form></login-form>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import logo from './assets/favicon.svg';
-import { getFarmbot, post } from './http';
-import SockJS from 'sockjs-client';
+import ControlRoom from './ControlRoom';
+import LoginForm from './LoginForm';
 
 export default {
-  name: 'app',
-  components: {},
+  components: {
+    ControlRoom,
+    LoginForm,
+  },
+  name: 'App',
   data() {
     return {
-      logo,
-      movementType: 'relative',
-      peripherals: [],
-      post,
-      sequences: [],
-      seqId: 13812,
-      toolId: undefined,
-      tools: [],
-      x: 0,
-      y: 0,
-      z: 0,
-    };
-  },
-  methods: {
-    move(x, y, z, movementType = 'relative') {
-      post(`/move/${movementType}`, { x: +x, y: +y, z: +z });
-    },
-    executeSequence() {
-      if (this.seqId) {
-        post(`/sequence/execute/${this.seqId}`);
-      }
-    },
-    async goToTool() {
-      const points = await getFarmbot('/points');
-      const { x, y, z } = points.find(p => p.tool_id === this.toolId);
-      this.move(x, y, z, 'absolute');
-    },
-    writePin(pinId, value) {
-      post(`/pin/${pinId}`, { value });
-    },
-  },
-  async mounted() {
-    this.tools = await getFarmbot('/tools');
-    this.peripherals = await getFarmbot('/peripherals');
-    this.sequences = await getFarmbot('/sequences');
-    this.toolId = this.tools[0].id;
-
-    const sockjs = new SockJS('http://localhost:3000/updates');
-
-    sockjs.onopen = () => console.log('[*] open', sockjs.protocol);
-    sockjs.onclose = () => console.log('[*] close');
-    sockjs.onmessage = e => {
-      const data = JSON.parse(e.data);
-      this.peripherals.forEach(per => {
-        Vue.set(per, 'value', !!data.pins[per.pin].value);
-      });
+      loggedIn: false,
     };
   },
 };
 </script>
 
-<style>
-body {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-.ui.form .ui.dropdown {
-  display: inline-block;
-  width: auto;
-}
-</style>
+<style scoped></style>
---
+++ src/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <sui-segment vertical>
+    <sui-container>
+      <h1>Login to my.farm.bot</h1>
+      <sui-form @submit.prevent="submit()">
+        <sui-form-field>
+          <label>E-mail address</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail address"
+            v-model="email"
+          />
+        </sui-form-field>
+        <sui-form-field>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            v-model="password"
+          />
+        </sui-form-field>
+
+        <sui-button primary>Login</sui-button>
+      </sui-form>
+    </sui-container>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  name: 'LoginForm',
+  methods: {
+    async submit() {
+      console.log(this.email, this.password);
+      const res = await fetch('https://my.farm.bot/api/tokens', {
+        method: 'POST',
+        body: JSON.stringify({
+          user: {
+            email: this.email,
+            password: this.password,
+          },
+        }),
+      });
+      console.log(res);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
